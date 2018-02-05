22bd339e9e48586d840f5320c344ecbfcf30d34c
maytenth <lane_butler@davincischools.org>
2018-02-05 15:20:25 -0800

    Add files via upload

---
+++ customsong.rb
@@ -0,0 +1,43 @@
+# Baby by Justin Bieber
+# Sheet Music: https://piano3sheets.weebly.com/uploads/4/3/3/7/4337601/baby-by-justin-bieber-music-sheet-piano.pdf
+# BPM Used : 126
+
+
+use_bpm 126
+use_synth :piano
+
+play :g4
+sleep 1
+play :f4
+sleep 1
+play :g4
+sleep 1
+play :f4
+sleep 0.5
+play :g4
+sleep 1
+play :g4
+sleep 0.5
+play :f4
+sleep 1
+play :bb4
+play :bb4
+sleep 1
+play :f4
+sleep 1
+play :g4
+sleep 1
+play :f4
+sleep 1
+play :g4
+sleep 0.5
+play :f4
+sleep 1
+play :g4
+sleep 1
+play :g4
+sleep 1
+play :f4
+sleep 1
+play :c4
+play :c4
